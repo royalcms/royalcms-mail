486eefbf49e6c51b8963c8ac38fed21a286d6f54
王正东 <wangzhengdong@ecmoban.com>
2020-04-14 16:47:58 +0800

    移动源码进src目录

---
+++ MailManager.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace Royalcms\Component\Mail;
+
+
+use ReflectionClass;
+use ReflectionMethod;
+
+class MailManager extends \Illuminate\Mail\MailManager
+{
+
+    /**
+     * Will create the instances.
+     *
+     * @royalcms 6.0.0
+     * @param $driver
+     */
+    public function resetDriver($driver)
+    {
+        // If the given driver has not been created before, we will create the instances
+        // here and cache it so we can return it next time very quickly. If there is
+        // already a driver created by this name, we'll just return that instance.
+        if (isset($this->mailers[$driver])) {
+            unset($this->mailers[$driver]);
+            $this->mailers[$driver] = $this->resolve($driver);
+        }
+    }
+
+    /**
+     * Mix another object into the class.
+     *
+     * @royalcms 6.0.0
+     * @param  object  $mixin
+     * @param  bool  $replace
+     * @return void
+     *
+     * @throws \ReflectionException
+     */
+    public function mixin($mixin, $replace = true)
+    {
+        $methods = (new ReflectionClass($mixin))->getMethods(
+            ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED
+        );
+
+        foreach ($methods as $method) {
+            if ($replace || ! isset($this->customCreators[$method->name])) {
+                $method->setAccessible(true);
+                $this->extend($method->name, $method->invoke($mixin));
+            }
+        }
+    }
+
+
+}
